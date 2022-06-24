8cc51bcf0bc5385f578c0a0e65fe3f3e1bb4492a
Mariusz112 <maniek11744@gmail.com>
2022-06-24 22:57:50 +0200

    server side validations/bootstrap

--- LibaryWeb/Pages/Categories/Create.cshtml.cs
+++ LibaryWeb/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibaryWeb.Model;
+using LibaryWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LibaryWeb.Pages.Categories
+namespace LibaryWeb.Pages.Categories;
+
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly ApplicationDbContext _db;
+    [BindProperty]
+    public Category Category { get; set; }
+
+    public CreateModel(ApplicationDbContext db)
     {
-        public void OnGet()
+        _db = db;
+    }
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
         {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
+        return Page();
     }
 }
